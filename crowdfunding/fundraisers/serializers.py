f3b46de67b543ea72204075b18ccaa95be401754
Arrianne <arrianneoshea@gmail.com>
2025-11-11 18:55:26 +1000

    wrote our first DRF serializer

---
+++ crowdfunding/fundraisers/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from django.apps import apps
+ 
+class FundraiserSerializer(serializers.ModelSerializer):
+    # The only thing we need to do is specify which model to convnert and which fields it should include below.
+    class Meta:
+       model = apps.get_model('fundraisers.Fundraiser')
+       fields = '__all__'
