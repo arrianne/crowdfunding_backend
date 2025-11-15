d772558af1f05c021ffbcfab90ce1ac638bf0bdb
Arrianne <arrianneoshea@gmail.com>
2025-11-15 11:55:38 +1000

    adding the pledge model, serializer, view and endpoint url

--- crowdfunding/fundraisers/serializers.py
+++ crowdfunding/fundraisers/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from django.apps import apps
- 
+
+class PledgeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = apps.get_model('fundraisers.Pledge')
+        fields = '__all__'
+
 class FundraiserSerializer(serializers.ModelSerializer):
     # The only thing we need to do is specify which model to convnert and which fields it should include below.
     class Meta:
-       model = apps.get_model('fundraisers.Fundraiser')
-       fields = '__all__'
+        model = apps.get_model('fundraisers.Fundraiser')
+        fields = '__all__'
+
+class FundraiserDetailSerializer(FundraiserSerializer):
+    pledges = PledgeSerializer(many=True, read_only=True)
--- crowdfunding/fundraisers/views.py
+++ crowdfunding/fundraisers/views.py
@@ -7,8 +7,9 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Fundraiser
-from .serializers import FundraiserSerializer
+from django.http import Http404
+from .models import Fundraiser, Pledge
+from .serializers import FundraiserSerializer, PledgeSerializer, FundraiserDetailSerializer
 
 class FundraiserList(APIView):
     # defines the behaviour we want our view to execute when it receives an HTTP GET request.
@@ -16,7 +17,7 @@ class FundraiserList(APIView):
         # use the Fundraiser model to get a list of all fundraisers in the database
         fundraisers = Fundraiser.objects.all()
         # use the FundraiserSerializer to convert that list to JSON
-        serializer = FundraiserSerializer(fundraisers, many=True)
+        serializer = FundraiserDetailSerializer(fundraisers, many=True)
         # return a response containing the serialized data.
         return Response(serializer.data)
 
@@ -34,3 +35,36 @@ class FundraiserList(APIView):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
     )
+class FundraiserDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            fundraiser = Fundraiser.objects.get(pk=pk)
+            return fundraiser
+        except Fundraiser.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        fundraiser = self.get_object(pk)
+        serializer = FundraiserDetailSerializer(fundraiser)
+        return Response(serializer.data)
+
+class PledgeList(APIView):
+
+    def get(self, request):
+        pledges = Pledge.objects.all()
+        serializer = PledgeSerializer(pledges, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PledgeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
