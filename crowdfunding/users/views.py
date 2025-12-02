c45e1d255be295d5e4f96f3dd8e2b69107c811f4
Arrianne <arrianneoshea@gmail.com>
2025-12-02 19:41:41 +1000

    fixing an irrelevant line of code in serializer which points to owner which the user doesn't have - they are the owner already

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -19,7 +19,7 @@ class CustomUserList(APIView):
     def post(self, request):
         serializer = CustomUserSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(owner=request.user)
+            serializer.save()
             return Response(
                 serializer.data,
                 status=status.HTTP_201_CREATED
