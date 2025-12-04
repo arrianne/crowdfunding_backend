2bb9b21de5a706f965c9891b7ff590e5a33f36e4
Arrianne <arrianneoshea@gmail.com>
2025-12-04 14:18:01 +1000

    removing incorrect admin logic from admin.py

--- crowdfunding/users/admin.py
+++ crowdfunding/users/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
 
-
-from django.contrib import admin
-from .models import Fundraiser, Pledge
-
-admin.site.register(Fundraiser)
-admin.site.register(Pledge)
+User = get_user_model()
+admin.site.register(User)
