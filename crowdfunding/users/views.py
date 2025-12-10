a2ad2a8b6790b7081d1620873c8f2c1a21ff2e39
Arrianne <arrianneoshea@gmail.com>
2025-12-10 12:18:40 +1000

    making sure only is_staff users can view the list of all users for security

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -4,13 +4,25 @@ from django.shortcuts import render
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
 class CustomUserList(APIView):
+    """
+    GET: Only admin users can list all users.
+    POST: Anyone can register a new user.
+    """
+
+    def get_permissions(self):
+        # Only admins can list all users
+        if self.request.method == "GET":
+            return [permissions.IsAdminUser()]
+        # For POST (registration), allow anyone
+        return []
+
     def get(self, request):
         users = CustomUser.objects.all()
         serializer = CustomUserSerializer(users, many=True)
@@ -30,17 +42,25 @@ class CustomUserList(APIView):
         )
 
 class CustomUserDetail(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
     def get_object(self, pk):
         try:
-            return CustomUser.objects.get(pk=pk)
+            user = CustomUser.objects.get(pk=pk)
         except CustomUser.DoesNotExist:
             raise Http404
+
+        # Allow if the requester is the user themselves or an admin
+        if self.request.user == user or self.request.user.is_staff:
+            return user
+
+        # Otherwise, pretend the user does not exist - prevents user ID enumeration.
+        raise Http404
 
     def get(self, request, pk):
         user = self.get_object(pk)
         serializer = CustomUserSerializer(user)
         return Response(serializer.data)
-    
 
 class CustomAuthToken(ObtainAuthToken):
     def post(self, request, *args, **kwargs):
@@ -55,5 +75,6 @@ class CustomAuthToken(ObtainAuthToken):
         return Response({
             'token': token.key,
             'user_id': user.id,
+            "username": user.username,
             'email': user.email
 })
